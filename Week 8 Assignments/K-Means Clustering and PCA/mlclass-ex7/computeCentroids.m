371dd00c63fdf962bd1f80209ff918e8c7ed211f
Dipanjan Sarkar <dipanzan.sarkar@gmail.com>
2014-06-17 16:05:36 +0530

    Added assignment 8 solutions

---
+++ Week 8 Assignments/K-Means Clustering and PCA/mlclass-ex7/computeCentroids.m
@@ -0,0 +1,46 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+
+
+for i = 1:K
+    c_i = idx==i;
+    n_i = sum(c_i);
+    c_i_matrix = repmat(c_i,1,n);
+    X_c_i = X .* c_i_matrix;
+    centroids(i,:) = sum(X_c_i) ./ n_i;
+end
+
+
+
+
+% =============================================================
+
+
+end
+
---
+++ Week 8 Assignments/K-Means Clustering and PCA/mlclass-ex7/findClosestCentroids.m
@@ -0,0 +1,42 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+m = size(X,1)
+
+for i = 1:m
+    distance_array = zeros(1,K);
+    for j = 1:K
+        distance_array(1,j) = sqrt(sum(power((X(i,:)-centroids(j,:)),2)));
+    end
+    [d, d_idx] = min(distance_array);
+    idx(i,1) = d_idx;
+end
+
+
+
+
+
+% =============================================================
+
+end
+
