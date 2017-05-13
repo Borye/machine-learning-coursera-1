a377a0bdf3e75997800b84f784c43a6c9e9f223f
Carl Jahn <carl.jahn@telekom.de>
2017-05-13 16:25:04 +0200

    updated gradientDescent files and removed the repmat calls

--- Week 2 Assignments/Linear Regression with Multiple Variables/mlclass-ex1/gradientDescent.m
+++ Week 2 Assignments/Linear Regression with Multiple Variables/mlclass-ex1/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -11,16 +11,13 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    
-    delta = (1/m)*sum(X.*repmat((X*theta - y), 1, size(X,2)));
-    
-    
-    theta = (theta' - (alpha * delta))';
+
+    theta = (theta' - ( alpha * ( 1 / m ) * sum( (X * theta - y) .* X) ))';
 
 
 
@@ -28,7 +25,7 @@ for iter = 1:num_iters
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
 
 end
