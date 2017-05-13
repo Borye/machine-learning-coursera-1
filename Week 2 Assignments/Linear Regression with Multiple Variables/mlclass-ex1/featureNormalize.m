3cd2497765cb7e5ad316b663e8f47942e8c70306
Carl Jahn <carl.jahn@telekom.de>
2017-05-13 15:26:23 +0200

    updated the feature normalize m file including an explanation why you dont need repmat

--- Week 2 Assignments/Linear Regression with Multiple Variables/mlclass-ex1/featureNormalize.m
+++ Week 2 Assignments/Linear Regression with Multiple Variables/mlclass-ex1/featureNormalize.m
@@ -1,5 +1,5 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
-%FEATURENORMALIZE Normalizes the features in X 
+%FEATURENORMALIZE Normalizes the features in X
 %   FEATURENORMALIZE(X) returns a normalized version of X where
 %   the mean value of each feature is 0 and the standard deviation
 %   is 1. This is often a good preprocessing step to do when
@@ -13,27 +13,30 @@ sigma = zeros(1, size(X, 2));
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
 %               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
+%               storing the mean value in mu. Next, compute the
 %               standard deviation of each feature and divide
 %               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
+%               the standard deviation in sigma.
 %
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
+%               Note that X is a matrix where each column is a
+%               feature and each row is an example. You need
+%               to perform the normalization separately for
+%               each feature.
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
+%
 
 
-mu = mean(X_norm);
-sigma = std(X_norm);
+mu = mean(X);
+sigma = std(X);
 
-tf_mu = X_norm - repmat(mu,length(X_norm),1);
-tf_std = repmat(sigma,length(X_norm),1);
+% Note: It is not really necessary to repmat the mu and sigma matrix
+% but if you want you can repmat those two
+% mu = repmat(mu, size(mu, 2), 1);
+% sigma = repmat(sigma, size(sigma, 2), 1);
 
-X_norm = tf_mu ./ tf_std;
+
+X_norm = (X - mu) ./ sigma;
 
 
 % ============================================================
